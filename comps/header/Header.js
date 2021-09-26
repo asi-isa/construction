f39a9e63dce66f9c9c27fa889f7b3e6afab87806
asi-isa <ali.inceoglu@protonmail.com>
2021-09-26 19:39:19 +0200

    header with ham

---
+++ comps/header/Header.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import styles from "./Header.module.css";
+
+export default function Header() {
+  const [showHamMenu, setShowHamMenu] = useState(false);
+  function toggleHamMenu() {
+    setShowHamMenu(!showHamMenu);
+  }
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.firm}>a.s.i.</h1>
+      <div
+        className={`${styles.ham} ${showHamMenu && styles.active}`}
+        onClick={toggleHamMenu}
+      ></div>
+      {/* <nav className={`${styles.nav} ${!showHamMenu && "none"}`}>
+        <ul className={styles.ul}>
+          <li className={styles.li}>meine arbeit</li>
+          <li className={styles.li}>über mich</li>
+          <li className={styles.li}>kontakt</li>
+          <li className={styles.li}>jobs</li>
+        </ul>
+      </nav> */}
+    </header>
+  );
+}
