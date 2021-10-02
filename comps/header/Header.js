e79d9512bfbd7e5cbf902d0198d42bcc5dcc1e72
asi-isa <ali.inceoglu@protonmail.com>
2021-10-02 14:02:33 +0200

    hamMenu modal

--- comps/header/Header.js
+++ comps/header/Header.js
@@ -8,24 +8,27 @@ export default function Header() {
     setShowHamMenu(!showHamMenu);
   }
   return (
-    <header className={styles.header}>
-      <h1 className={styles.firm}>a.s.i.</h1>
-      <div
-        className={`${styles.ham} ${showHamMenu && styles.active}`}
-        onClick={toggleHamMenu}
-      ></div>
-      <nav className={`${styles.nav} ${!showHamMenu && "transparent"}`}>
-        <ul className={styles.ul}>
-          <li className={styles.li}>
-            <Link href="/">
-              <a className={styles.link}>Leistungen</a>
-            </Link>
-          </li>
-          <li className={styles.li}>über mich</li>
-          <li className={styles.li}>kontakt</li>
-          <li className={styles.li}>jobs</li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      {showHamMenu && <div className={styles.modal}></div>}
+      <header className={styles.header}>
+        <h1 className={styles.firm}>a.s.i.</h1>
+        <div
+          className={`${styles.ham} ${showHamMenu && styles.active}`}
+          onClick={toggleHamMenu}
+        ></div>
+        <nav className={`${styles.nav} ${!showHamMenu && "transparent"}`}>
+          <ul className={styles.ul}>
+            <li className={styles.li}>
+              <Link href="/">
+                <a className={styles.link}>Leistungen</a>
+              </Link>
+            </li>
+            <li className={styles.li}>über mich</li>
+            <li className={styles.li}>kontakt</li>
+            <li className={styles.li}>jobs</li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
