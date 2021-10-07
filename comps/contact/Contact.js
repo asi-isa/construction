b7a12b0d08adcba85a368999e9de82edee8e8496
asi-isa <ali.inceoglu@protonmail.com>
2021-10-07 13:22:50 +0200

    send formData

--- comps/contact/Contact.js
+++ comps/contact/Contact.js
@@ -2,10 +2,14 @@ import styles from "./Contact.module.css";
 import { AiOutlinePhone, AiOutlineMail } from "react-icons/ai";
 import { IoLocationOutline } from "react-icons/io5";
 import { FiFacebook, FiInstagram, FiLinkedin } from "react-icons/fi";
+import Loader from "../loader/Loader";
+import { useState } from "react";
 // import { BsMailbox2 } from "react-icons/bs";
 
 export default function Contact() {
-  function formHandler(e) {
+  const [sending, setSending] = useState(false);
+
+  async function formHandler(e) {
     e.preventDefault();
 
     const formData = {};
@@ -14,119 +18,134 @@ export default function Contact() {
       if (formElement.value) formData[formElement.name] = formElement.value;
     });
 
-    console.log(formData);
+    try {
+      setSending(true);
+      await fetch("/api/mail", {
+        method: "post",
+        body: JSON.stringify(formData),
+      });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setSending(false);
+    }
   }
+
   return (
-    <section className={styles.contact}>
-      <div className={styles.header}>
-        <h2 className={styles.title}>Wir freuen uns auf Ihre Nachricht</h2>
-        <h4 className={styles.subtitle}>
-          Haben Sie noch Fragen? Dann zögern Sie nicht uns eine Mitteilung zu
-          senden.
-        </h4>
-      </div>
-
-      <div className={styles.contact_form}>
-        <div className={styles.contact_information}>
-          <h2 className={styles.contact_info_title}>
-            Unsere Kontaktinformationen
-          </h2>
-
-          <div className={styles.contact_info_con}>
-            <p className={styles.contact_info}>
-              <AiOutlinePhone className={styles.icon} /> +49 1514 24242424
-            </p>
-            <p className={styles.contact_info}>
-              <AiOutlineMail className={styles.icon} /> isa.tech@email.com
-            </p>
-            <p className={styles.contact_info}>
-              <IoLocationOutline className={styles.icon} /> somestreet 42, 21212
-              some city
-            </p>
-          </div>
-
-          <div className={styles.social_media}>
-            <FiFacebook />
-            <FiInstagram />
-            <FiLinkedin />
-          </div>
+    <>
+      <section className={styles.contact}>
+        <div className={styles.header}>
+          <h2 className={styles.title}>Wir freuen uns auf Ihre Nachricht</h2>
+          <h4 className={styles.subtitle}>
+            Haben Sie noch Fragen? Dann zögern Sie nicht uns eine Mitteilung zu
+            senden.
+          </h4>
         </div>
 
-        <form className={styles.form} onSubmit={formHandler}>
-          <div className={styles.col_two}>
-            <div className={styles.col}>
-              <label htmlFor="firstName" className={styles.label}>
-                Vorname
-              </label>
-              <input
-                type="text"
-                id="firstName"
-                name="firstName"
-                className={styles.input}
-                placeholder="Vorname"
-                required
-              />
+        <div className={styles.contact_form}>
+          <div className={styles.contact_information}>
+            <h2 className={styles.contact_info_title}>
+              Unsere Kontaktinformationen
+            </h2>
+
+            <div className={styles.contact_info_con}>
+              <p className={styles.contact_info}>
+                <AiOutlinePhone className={styles.icon} /> +49 1514 24242424
+              </p>
+              <p className={styles.contact_info}>
+                <AiOutlineMail className={styles.icon} /> isa.tech@email.com
+              </p>
+              <p className={styles.contact_info}>
+                <IoLocationOutline className={styles.icon} /> somestreet 42,
+                21212 some city
+              </p>
             </div>
-            <div className={styles.col}>
-              <label htmlFor="lastName" className={styles.label}>
-                Nachname
-              </label>
-              <input
-                type="text"
-                id="lastName"
-                name="lastName"
-                className={styles.input}
-                placeholder="Nachname"
-                required
-              />
+
+            <div className={styles.social_media}>
+              <FiFacebook />
+              <FiInstagram />
+              <FiLinkedin />
             </div>
           </div>
 
-          <div className={styles.col_two}>
-            <div className={styles.col}>
-              <label htmlFor="mail" className={styles.label}>
-                Email
-              </label>
-              <input
-                type="email"
-                id="mail"
-                name="mail"
-                className={styles.input}
-                placeholder="Email"
-                required
-              />
+          <form className={styles.form} onSubmit={formHandler}>
+            <div className={styles.col_two}>
+              <div className={styles.col}>
+                <label htmlFor="firstName" className={styles.label}>
+                  Vorname
+                </label>
+                <input
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                  className={styles.input}
+                  placeholder="Vorname"
+                  required
+                />
+              </div>
+              <div className={styles.col}>
+                <label htmlFor="lastName" className={styles.label}>
+                  Nachname
+                </label>
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  className={styles.input}
+                  placeholder="Nachname"
+                  required
+                />
+              </div>
             </div>
-            <div className={styles.col}>
-              <label htmlFor="phone" className={styles.label}>
-                Telefonnummer
-              </label>
-              <input
-                type="text"
-                id="phone"
-                name="phone"
-                className={styles.input}
-                placeholder="Telefonnummer"
-                required
-              />
+
+            <div className={styles.col_two}>
+              <div className={styles.col}>
+                <label htmlFor="mail" className={styles.label}>
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="mail"
+                  name="mail"
+                  className={styles.input}
+                  placeholder="Email"
+                  required
+                />
+              </div>
+              <div className={styles.col}>
+                <label htmlFor="phone" className={styles.label}>
+                  Telefonnummer
+                </label>
+                <input
+                  type="text"
+                  id="phone"
+                  name="phone"
+                  className={styles.input}
+                  placeholder="Telefonnummer"
+                  required
+                />
+              </div>
             </div>
-          </div>
 
-          <label htmlFor="message" className={styles.label}>
-            Nachricht
-          </label>
+            <label htmlFor="message" className={styles.label}>
+              Nachricht
+            </label>
 
-          <textarea
-            name="message"
-            id="message"
-            rows="7"
-            className={`${styles.input} ${styles.textarea}`}
-            placeholder="Nachricht"
-            required
-          ></textarea>
+            <textarea
+              name="message"
+              id="message"
+              rows="7"
+              className={`${styles.input} ${styles.textarea}`}
+              placeholder="Nachricht"
+              required
+            ></textarea>
 
-          <button className={styles.btn}>Senden</button>
-        </form>
-      </div>
-    </section>
+            <button className={styles.btn}>Senden</button>
+          </form>
+        </div>
+      </section>
+
+      {sending && <Loader />}
+    </>
   );
 }
---
+++ pages/api/mail.js
@@ -0,0 +1,28 @@
+const mail = require("@sendgrid/mail");
+mail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export default async function (req, res) {
+  const body = JSON.parse(req.body);
+
+  const message = `
+    Email: ${body.mail}\r\n
+    Phone: ${body.phone}\r\n
+    Message: ${body.message}
+  `;
+
+  const msg = {
+    to: "ali.inceoglu94@gmail.com",
+    from: "info@moneyo.tech", // Use the email address or domain you verified above
+    subject: `Req from ${body.firstName} ${body.lastName}`,
+    text: message,
+    html: message.replace("/\r\n/g", "<br>"),
+  };
+
+  try {
+    await mail.send(msg);
+  } catch (error) {
+    console.error(error);
+  }
+
+  res.status(200).json({ status: "ok" });
+}
